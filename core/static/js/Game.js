692fcfd102f49686245583513b6d6cd68a0a34d1
dzordich <dzordich@gmail.com>
2019-08-27 20:47:44 -0400

    animation/player speed tweaks

--- core/static/js/Game.js
+++ core/static/js/Game.js
@@ -26,32 +26,38 @@ DarkHorizon.Game.prototype = {
         this.player.animations.add('stand-left', [8, 9, 10, 11, 12], 9, true);
         this.player.animations.add('run-right', [16, 17, 18, 19, 20, 21, 22, 23], 18, true);
         this.player.animations.add('run-left', [24, 25, 26, 27, 28, 29, 30, 31], 18, true);
+        this.player.animations.add('jump-right', [22], 1, true);
+        this.player.animations.add('jump-left', [30], 1, true);
 
         
 
         this.game.physics.arcade.enable(this.player);
         this.player.body.collideWorldBounds = true;
         this.game.camera.follow(this.player);
-        this.player.body.gravity.y = 500;
+        this.player.body.gravity.y = 800;
 
         this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.rightKey = this.game.input.keyboard.addKey(Phaser.KeyCode.D);
+        this.leftKey = this.game.input.keyboard.addKey(Phaser.KeyCode.A);
+        this.jumpKey = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
 
     },
     update: function(){
         console.log(this.player.centerX, this.player.centerY)
         this.game.physics.arcade.collide(this.player, this.blockedLayer, this.resetJumps, null, this);
 
-        let left = this.cursors.left.isDown;
-        let right = this.cursors.right.isDown;
+        let left = this.cursors.left.isDown || this.leftKey.isDown;
+        let right = this.cursors.right.isDown || this.rightKey.isDown;
+
         if(right){
             FACING_RIGHT = true;
             this.player.animations.play('run-right');
-            this.player.body.velocity.x = 130;
+            this.player.body.velocity.x = 170;
         }
         else if(left){
             FACING_RIGHT = false;
             this.player.animations.play('run-left');
-            this.player.body.velocity.x = -130;
+            this.player.body.velocity.x = -170;
         }
         else{
             if(FACING_RIGHT){
@@ -62,17 +68,29 @@ DarkHorizon.Game.prototype = {
             }
             this.player.body.velocity.x = 0;
         }
-        this.cursors.up.onDown.add(this.jump, this);
+        this.jumpKey.onDown.add(this.jump, this);
     },
     jump: function(){
         if(JUMPS < 2){
             // this.player.animations.paused = true;
-            this.player.body.velocity.y = -300;
+            this.player.body.velocity.y = -320;
             JUMPS++;
+            if (!FACING_RIGHT){
+                this.player.animations.play('jump-left');
+            }
+            else {
+                this.player.animations.play('jump-right');
+            }
         }
     },
     resetJumps: function(player, body){
         JUMPS = 0;
+        // if (!FACING_RIGHT) {
+        //     this.player.animations.play('jump-left');
+        // }
+        // else {
+        //     this.player.animations.play('jump-right');
+        // }
     },
     chase: function(sprite){
         if(sprite.centerX > this.player.centerX){
